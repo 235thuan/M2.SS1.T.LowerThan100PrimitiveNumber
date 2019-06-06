b3947ca37862fe9a12c9f14280797cc5c708fe74
thuannguyen89 <235thuan@gmail.com>
2019-06-06 17:23:08 +0700

    .

--- src/main/java/LowerThan100PrimitiveNumber.java
+++ src/main/java/LowerThan100PrimitiveNumber.java
@@ -1,25 +1,45 @@
-import java.util.Scanner;
-
 public class LowerThan100PrimitiveNumber {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int limit = 100;
-        int[] array = {};
-        array = new int[limit];
-        for (int i = 2; i < limit; i++) {
-            array[i] = i;
-            boolean check =true;
-            int squareRoot=(int) Math.round(Math.sqrt(i));
-            int j = 2;
-            while (j <= squareRoot ) {
-                if (i%j==0) {
-                    check = false;
+        int count = 0;
+        int i = 0;
+        int number = 100;
+        for (; i < number; i++) {
+            boolean isPrime = true;
+            if (count < 2) isPrime = false;
+            for (int j = 2; j <= Math.sqrt(count); j++) {
+                if (count % j == 0) {
+                    isPrime = false;
                     break;
                 }
-                j++;
             }
-            if(check)
-            System.out.print(array[i]+" ");
+            if (isPrime) {
+                System.out.print(count + " ");
+                i++;
+            }
+            count++;
         }
+
+
+
+
+//        int limit = 100;
+//        int[] array;
+//        array = new int[limit];
+//        for (int i = 2; i < limit; i++) {
+//            array[i] = i;
+//            boolean check =true;
+//            int squareRoot=(int) Math.round(Math.sqrt(i));
+//            int j = 2;
+//            while (j <= squareRoot ) {
+//                if (i%j==0) {
+//                    check = false;
+//                    break;
+//                }
+//                j++;
+//            }
+//            if(check) {
+//                System.out.print(array[i] + " ");
+//            }
+//        }
     }
 }
